fcc720819a413dacc9631eac780840b104701dd2
Udit26085 <bhatiaudit55@gmail.com>
2025-12-24 18:33:13 +0530

    Add static file settings and update requirements.txt

--- internship_assignment_for_bne/settings.py
+++ internship_assignment_for_bne/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'app',
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,14 +124,9 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "app" / "static",
-]
